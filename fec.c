63b84ae4959e34f052b7d4b666d32733ac8cf47c
hanishkvc <hanishkvc@gmail.com>
2016-08-19 19:24:02 +0530

    Operate on a given file
    * Added a constant to define the actual matrix buffer size.
      NOTE: This includes the data blocks as well as fec data blocks
      at end of each data row and column
    
    * Added a function to load a given fec buffer with data matrix
      from the given file.
    
    * Added type casting for the buffer being processed using intrinsic
      simd instruction, to avoid the warnings.
    
    * Updated makefile to include debug info in the executable

--- fec.c
+++ fec.c
@@ -2,9 +2,15 @@
 #include <stdint.h>
 #include <emmintrin.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 #define FEC_BLOCKSIZE 4096
 #define FEC_DATAMATRIX1D 8
 #define FEC_FULLMATRIX ((FEC_DATAMATRIX1D+1)*(FEC_DATAMATRIX1D+1))
+#define FEC_BUFFERSIZE (FEC_FULLMATRIX*FEC_BLOCKSIZE)
 
 int fec_validmeta(int blocksize, int matrix)
 {
@@ -24,20 +30,49 @@ void fec_genfec(uint8_t *buf, int blocksize, int matrix)
 		for(int i = 0; i < blocksize; i+= 16) {
 			res = _mm_setzero_si128();
 			for(int x = 0; x < matrix; x++) {
-				val = _mm_loadu_si128(&buf[iCurRowOffset+x*blocksize+i*16]);
+				val = _mm_loadu_si128((__m128i*)&buf[iCurRowOffset+x*blocksize+i*16]);
 				res = _mm_xor_si128(res, val);
 			}
-			_mm_storeu_si128(&buf[iCurRowOffset+matrix*blocksize+i*16], res);
+			_mm_storeu_si128((__m128i*)&buf[iCurRowOffset+matrix*blocksize+i*16], res);
 		}
 	}
 }
 
+int fec_loadbuf(uint8_t *buf, int hFile, int blocksize, int matrix)
+{
+	int iRead;
+	int iCurRowOffset;
+	int iBufSizeDataRow = matrix*blocksize;
+	for(int y = 0; y < matrix; y++) {
+		iCurRowOffset = y*(matrix+1)*blocksize;
+		iRead = read(hFile, &buf[iCurRowOffset], iBufSizeDataRow);
+		if (iRead != iBufSizeDataRow)
+			return -1;
+	}
+	return 0;
+}
 
 int main(int argc, char **argv)
 {
 
-	uint8_t buf[FEC_FULLMATRIX*FEC_BLOCKSIZE];
+	uint8_t buf[FEC_BUFFERSIZE];
 
-	fec_genfec(buf, FEC_BLOCKSIZE, FEC_DATAMATRIX1D);
+	int hFSrc, hFDst;
+	int iMatrix;
+
+	hFSrc = open(argv[1],O_RDONLY);
+	hFDst = open(argv[2],O_WRONLY);
+
+	iMatrix = 0;
+	while(1) {
+		if (fec_loadbuf(buf, hFSrc, FEC_BLOCKSIZE, FEC_DATAMATRIX1D) != 0) {
+			break;
+		} else {
+			printf("FEC:INFO: processing data matrix [%d]\n", iMatrix);
+		}
+		fec_genfec(buf, FEC_BLOCKSIZE, FEC_DATAMATRIX1D);
+		write(hFDst, buf, FEC_BUFFERSIZE);
+	}
 	return 0;
 }
+
