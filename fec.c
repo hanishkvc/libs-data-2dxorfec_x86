843a58ac2a94f9fc5abdd027cc1063053e9a3173
hanishkvc <hanishkvc@gmail.com>
2016-08-19 18:34:44 +0530

    Logical clarity
    * FEC_DATAMATRIX1D gives the number of data blocks in a given row or
      column
      NOTE: We work with square matrixes only currently
    
    * FEC_FULLMATRIX gives the total number of blocks in the matrix, this
      includes the data blocks as well as the fec blocks at the end of
      each row and column of blocks in the 2D matrix.
    
    * Added a variable to specify the starting offset with in the buffer
      of the current row of blocks being processed.
    
    * Added a main program which calls the functions provided by this
      fec library.

--- fec.c
+++ fec.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 #define FEC_BLOCKSIZE 4096
-#define FEC_MATRIX 8
+#define FEC_DATAMATRIX1D 8
+#define FEC_FULLMATRIX ((FEC_DATAMTRIX1D+1)*(FEC_DATAMTRIX1D+1))
 
 int fec_validmeta(int blocksize, int matrix)
 {
@@ -14,16 +15,27 @@ int fec_validmeta(int blocksize, int matrix)
 void fec_genfec(uint8 *buf, int blocksize, int matrix)
 {
 	__m128i res, val;
+	int iCurRowOffset;
 
 	for(y = 0; y < matrix; y++) {
+		iCurRowOffset = y*(matrix+1)*blocksize;
 		for(i = 0; i < blocksize; i+= 16) {
 			res = 0;
 			for(x = 0; x < matrix; x++) {
-				val = __mm_loadu_si128(buf[y*blocksize*(matrix+1)+x*blocksize+i*16]);
+				val = __mm_loadu_si128(buf[iCurRowOffset+x*blocksize+i*16]);
 				res = __mm_xor_si128(res, val);
 			}
-			__mm_storeu_si128(buf[y*blocksize*(matrix+1)+matrix*blocksize+i*16], res);
+			__mm_storeu_si128(buf[iCurRowOffset+matrix*blocksize+i*16], res);
 		}
 	}
 }
 
+
+int main(int argc, char **argv)
+{
+
+	buf[FEC_FULLMATRIX*FEC_BLOCKSIZE];
+
+	fec_genfec(buf, FEC_BLOCKSIZE, FEC_DATAMATRIX1D);
+	return 0;
+}
